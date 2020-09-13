5560b781526574b3ea90b7ac667e1ce0e7c664f4
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2020-09-12 22:23:03 -0400

    ✨ Convert to Typescript

---
+++ learn-white-hat/src/Terminal/Terminal.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Terminal.css';
+
+class Terminal extends React.Component<{}, {}> {
+    render() {
+        return (
+            <div className="Terminal">
+                Terminal
+            </div>
+        )
+    }
+}
+    
+
+export default Terminal;
